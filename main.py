06f62bcfbb67a3569366d66ae305d822db1d8dc6
Eldan <a.eldan@alem.kz>
2022-01-25 12:34:08 +0600

    Allow CORS

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from db import database
 from resources.routes import api_router
 
+origins = [
+    "http://localhost",
+    "http://localhost:8000",
+]
+
 app = FastAPI()
 app.include_router(api_router)
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.on_event("startup")
 async def startup():
@@ -20,8 +32,3 @@ async def shutdown():
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
